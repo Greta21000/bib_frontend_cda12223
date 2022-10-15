61650bd2d56476ec2aaeb13578cee6e7ea13ac1b
laurent-aubry <46169816+laurent-aubry@users.noreply.github.com>
2022-10-15 20:48:27 +0200

    Update CardList.jsx

--- src/card-list/CardList.jsx
+++ src/card-list/CardList.jsx
@@ -3,7 +3,7 @@ import "./CardList.css";
 
 const CardList = (props) => {
   return (
-    <div className="card-item" >
+    <div className="card-list" >
       {props.oeuvres.map((o) => (
         <CardItem key={o.id} oeuvre={o} />
       ))}
